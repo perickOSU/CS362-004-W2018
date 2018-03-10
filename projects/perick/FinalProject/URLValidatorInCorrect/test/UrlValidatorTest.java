083814ddd22021bd4d1485af6e18cb1341c1778a
perickOSU <perick@oregonstate.edu>
2018-03-10 12:57:02 -0600

    added manual tests.  found bug:  doesn't recognize that 'http:/' is invalid.

---
+++ projects/perick/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -0,0 +1,54 @@
+
+
+import junit.framework.TestCase;
+
+//You can use this as a skeleton for your 3 different test approach
+//It is an optional to use this file, you can generate your own test file(s) to test the target function!
+// Again, it is up to you to use this file or not!
+
+
+
+
+
+public class UrlValidatorTest extends TestCase {
+
+
+   public UrlValidatorTest(String testName) {
+      super(testName);
+   }
+
+   
+   
+   public void testManualTest()
+   {
+	   /* Various manual test cases: */
+	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   assertTrue(urlVal.isValid("http://www.google.com"));
+	   assertFalse(urlVal.isValid("http:/www.google.com"));
+
+   }
+   
+   
+   public void testYourFirstPartition()
+   {
+	 //You can use this function to implement your First Partition testing	   
+
+   }
+   
+   public void testYourSecondPartition(){
+		 //You can use this function to implement your Second Partition testing	   
+
+   }
+   //You need to create more test cases for your Partitions if you need to 
+   
+   public void testIsValid()
+   {
+	   //You can use this function for programming based testing
+
+   }
+   
+
+
+}
